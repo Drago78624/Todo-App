ef1f7a5545375e25d9aba30f12b9fc7e205c1f1f
Muhammad Maaz Ahmed <60292456+Drago78624@users.noreply.github.com>
2022-06-02 05:12:56 +0000

    dropdown completed

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const input = document.querySelector("input");
 const todosContainer = document.querySelector(".todos-container");
 const plusIcon = document.querySelector(".plus-icon");
+const dropdownBtn = document.querySelector(".dropdown-btn");
+const dropdown = document.querySelector(".todo-filter-dropdown");
+const filter = document.querySelector(".filter");
 const completeBtn = document.querySelectorAll(".complete");
 const deleteBtns = document.querySelectorAll(".delete");
+const options = document.querySelectorAll(".option");
+
+const todos = [];
 
 plusIcon.addEventListener("click", () => {
   if (input.value != "") {
@@ -17,6 +23,7 @@ plusIcon.addEventListener("click", () => {
           `;
     todosContainer.insertAdjacentHTML("beforeend", html);
     input.value = "";
+    
   }
 });
 
@@ -28,8 +35,28 @@ todosContainer.addEventListener("click", (e) => {
   } else if (e.target.classList.contains("complete")) {
     console.log(e.target.previousElementSibling);
     e.target.previousElementSibling.style.textDecoration = "line-through";
+    e.target.previousElementSibling.classList.add("completed")
+    e.target.disabled = true
+    
+    
   } else if (e.target.classList.contains("fa-check")) {
     e.target.parentElement.previousElementSibling.style.textDecoration =
       "line-through";
+    e.target.parentElement.previousElementSibling.classList.add("completed")
+    e.target.disabled = true
+    
   }
 });
+
+dropdownBtn.addEventListener("click", ()=>{
+  dropdown.classList.toggle("show")
+})
+
+options.forEach(option => {
+  option.addEventListener("click", (e) => {
+    console.log(e.target.innerText)
+    filter.innerText = e.target.innerText
+  dropdown.classList.remove("show")
+    
+  })
+})
